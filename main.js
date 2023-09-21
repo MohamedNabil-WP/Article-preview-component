86d77b0505becd6581e96a71f3026f289aaa2537
MohamedNabil-WP <142003857+MohamedNabil-WP@users.noreply.github.com>
2023-09-21 15:17:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const share = document.querySelector(".imgShare");
+
+const hidden = document.querySelector(".hidden");
+
+share.addEventListener("click", () => {
+
+    hidden.classList.toggle("hidden");
+
+})
+
